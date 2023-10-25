edc9ae1154ee02cc58fbb28625d7d3ebfecfbc4d
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-25 00:19:55 +0000

    donezo

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,22 @@
 class GameController < ApplicationController
 
   def home
-
     render({ :template => "my_templates/go_home" })
   end
 
+  def play
+    @number = params.fetch("number").to_i
+
+    @sides = params.fetch("sides").to_i
+
+    @rolls = []
+
+    @number.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+
+    render({ :template => "my_templates/press_play" })
+  end
 end
